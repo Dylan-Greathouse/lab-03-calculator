161463bd355c63391f0d3931211ac0ec7dc9f77d
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-06-03 14:57:21 -0700

    working add feature

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 // import functions
+import { add } from './calculations.js';
 
 // reference needed DOM elements
 const addXInput = document.getElementById('add-x-input');
 const addYInput = document.getElementById('add-y-input');
 const addButton = document.getElementById('add-button');
-const addResult = document.getElementById('add-results');
+const addResult = document.getElementById('add-result');
 
 
 
 // set event listeners
 addButton.addEventListener('click', () =>{
-  console.log('add button clicked');
+    const x = Number(addXInput.value);
+    const y = Number(addYInput.value);
+
+    const sum = add(x, y);
+
+    addResult.textContent = sum;
 });
   // get user input(s)
   // do any needed work with the value(s)
