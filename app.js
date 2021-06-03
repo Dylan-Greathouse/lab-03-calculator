95731c4dde5f6426850455934b84eb46e749da40
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-06-03 14:11:38 -0700

    add event listener

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // import functions
 
 // reference needed DOM elements
+const addXInput = document.getElementById('add-x-input');
+const addYInput = document.getElementById('add-y-input');
+const addButton = document.getElementById('add-button');
+const addResult = document.getElementById('add-results');
 
 
-// set event listeners 
+
+// set event listeners
+addButton.addEventListener('click', () =>{
+  console.log('add button clicked');
+});
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
